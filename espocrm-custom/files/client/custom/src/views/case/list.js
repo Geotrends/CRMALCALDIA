830f7827d33f0e184cf7d1b7a320a331f92df428
Geotrends SAS <geotrends@Mac-mini-de-Geotrends-2.local>
2026-06-25 18:02:43 -0500

    errores

--- espocrm-custom/files/client/custom/src/views/case/list.js
+++ espocrm-custom/files/client/custom/src/views/case/list.js
@@ -16,6 +16,14 @@ define('custom:views/case/list', ['views/list'], function (Dep) {
 
                 this.menu.hiddenItemList = hidden;
             }
+
+            this.listenTo(this.collection, 'sync', function () {
+                this.decorateKanbanBoard();
+            });
+
+            this.listenTo(this, 'after:render', function () {
+                this.decorateKanbanBoard();
+            });
         },
 
         checkAccessAction: function (action) {
@@ -26,6 +34,32 @@ define('custom:views/case/list', ['views/list'], function (Dep) {
             return Dep.prototype.checkAccessAction
                 ? Dep.prototype.checkAccessAction.call(this, action)
                 : true;
+        },
+
+        decorateKanbanBoard: function () {
+            if (!this.$el.hasClass('list-kanban')) {
+                return;
+            }
+
+            var $headers = this.$el.find('.kanban-head-container th.group-header');
+
+            this.$el.find('td.group-column').each(function (index) {
+                var $column = $(this);
+                var count = $column.find('.item').length;
+                var $header = $headers.eq(index);
+                var $label = $header.find('.kanban-group-label');
+
+                if (!$label.length) {
+                    return;
+                }
+
+                var baseLabel = String($label.attr('data-base-label') || $label.text())
+                    .replace(/\s*\(\d+\)\s*$/, '')
+                    .trim();
+
+                $label.attr('data-base-label', baseLabel);
+                $label.text(baseLabel + ' (' + count + ')');
+            });
         },
     });
 });
